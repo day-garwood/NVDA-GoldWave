a7ca34046cc99fde63e8b16c14beb2c27baa2298
Joseph Lee <joseph.lee22590@gmail.com>
2013-11-03 00:56:36 -0700

    Presets window: presets controls such as preview and stop are now recognized as buttons instead of unknown control type.

--- addon/appModules/goldwave.py
+++ addon/appModules/goldwave.py
@@ -10,6 +10,7 @@ import speech # No need for braille yet.
 from logHandler import log
 addonHandler.initTranslation()
 import scriptHandler
+from NVDAObjects.window import Window # Presets controls.
 
 class AppModule(appModuleHandler.AppModule):
 	
@@ -31,6 +32,14 @@ class AppModule(appModuleHandler.AppModule):
 	def soundWindow(self):
 		obj = api.getNavigatorObject()
 		return 1 if obj.name != "Workspace" else 0 # For now, nav obj's name is used; may use other attributes later.
+	
+	# Presets window: the various controls for presets are buttons, so let NVDA see them as such.
+	
+	def event_NVDAObject_init(self, obj):
+		if isinstance(obj, Window) and obj.windowClassName == "TBitton":
+			obj.role = 9 # Applies to presets control buttons.
+	
+	
 	
 	# Audio editing scripts:
 	
