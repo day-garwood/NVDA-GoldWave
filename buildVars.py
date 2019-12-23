280e90950b74779120e6982da46b7e3e46ddfa78
Joseph Lee <joseph.lee22590@gmail.com>
2019-12-23 10:32:43 -0800

    GoldWave 20.01
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("An add-on for enhancing access and usage of the GoldWave audio editor."),
 	# version
-	"addon_version" : "19.11",
+	"addon_version" : "20.01",
 	# Author(s)
 	"addon_author" : u"Joseph Lee <joseph.lee22590@gmail.com>, David Parduhn <xkill85@gmx.net>, Mesar Hameed <mhameed@src.gnome.org>",
 	# URL for the add-on documentation support
