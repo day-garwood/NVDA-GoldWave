1bcdaa3bf4ed6fee61077118f3a6c0fe060e149c
Joseph Lee <joseph.lee22590@gmail.com>
2021-07-25 11:38:38 -0700

    BuildVars: remove Mesar Hameed from authors list as email address is old

--- buildVars.py
+++ buildVars.py
@@ -27,7 +27,7 @@ addon_info = {
 	# version
 	"addon_version": "21.08",
 	# Author(s)
-	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>, David Parduhn <xkill85@gmx.net>, Mesar Hameed <mhameed@src.gnome.org>",
+	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>, David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
 	"addon_url": "https://addons.nvda-project.org",
 	# Documentation file name
