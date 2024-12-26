baa5d595f737f6e6102324a348504ad465c0040a
Joseph Lee <joseph.lee22590@gmail.com>
2024-12-26 07:50:00 -0700

    Install tasks: remove support for background updates performed by Add-on Updater

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -18,20 +18,19 @@ def onInstall():
 	# GoldWave add-on requires Windows 10 21H2 or later.
 	minimumWinVer = winVersion.WIN10_21H2
 	if currentWinVer < minimumWinVer:
-		if not globalVars.appArgs.minimal:
-			gui.messageBox(
-				_(
-					# Translators: Dialog text shown when trying to install the add-on on
-					# releases earlier than minimum supported release.
-					"You are using {releaseName} ({build}), a Windows release not supported by this add-on.\n"
-					"This add-on requires {supportedReleaseName} ({supportedBuild}) or later."
-				).format(
-					releaseName=currentWinVer.releaseName,
-					build=currentWinVer.build,
-					supportedReleaseName=minimumWinVer.releaseName,
-					supportedBuild=minimumWinVer.build
-				),
-				# Translators: dialog title shown when trying to install the add-on in unsupported systems.
-				_("Unsupported Windows release")
-			)
+		gui.messageBox(
+			_(
+				# Translators: Dialog text shown when trying to install the add-on on
+				# releases earlier than minimum supported release.
+				"You are using {releaseName} ({build}), a Windows release not supported by this add-on.\n"
+				"This add-on requires {supportedReleaseName} ({supportedBuild}) or later."
+			).format(
+				releaseName=currentWinVer.releaseName,
+				build=currentWinVer.build,
+				supportedReleaseName=minimumWinVer.releaseName,
+				supportedBuild=minimumWinVer.build
+			),
+			# Translators: dialog title shown when trying to install the add-on in unsupported systems.
+			_("Unsupported Windows release")
+		)
 		raise RuntimeError("Attempting to install GoldWave add-on on Windows releases earlier than 10")
