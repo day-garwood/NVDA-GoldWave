9d087bcba4d59ce535e225384e09294ec54f8a4e
Joseph Lee <joseph.lee22590@gmail.com>
2026-01-14 20:24:36 -0700

    Update copyright year

--- addon/appModules/goldwave.py
+++ addon/appModules/goldwave.py
@@ -1,6 +1,6 @@
 # GoldWave App Module
 # An add-on for NVDA
-# Copyright 2013-2025 Joseph Lee and contributors, released under gPL.
+# Copyright 2013-2026 Joseph Lee and contributors, released under gPL.
 # Functionality is based on JFW scripts for Goldwave by Jim Grimsby, Jr.
 
 import appModuleHandler
